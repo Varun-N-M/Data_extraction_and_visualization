2bf63b43699714659618634a71e869a4da75736a
Varun-N-M <114069624+Varun-N-M@users.noreply.github.com>
2023-04-01 11:57:57 +0530

    Add files via upload

---
+++ hotel_query.sql
@@ -0,0 +1,19 @@
+with hotels as (
+
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$']
+)
+select * from hotels
+join 
+dbo.market_segment$
+on 
+hotels.market_segment=market_segment$.market_segment
+left join
+dbo.meal_cost$
+on
+hotels.meal=meal_cost$.meal;
+
+
